17f7c13232b386c8b49e05017ccfec1c2750cf16
Lucas <lucasalves_taz@hotmail.com>
2026-01-23 23:58:31 -0300

    up flow_etl.py

--- flow_etl.py
+++ flow_etl.py
@@ -17,7 +17,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-DB_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DB_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 # Parâmetros da API
 PARAMS = {
